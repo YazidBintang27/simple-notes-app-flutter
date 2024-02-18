ac4ff606832c4a57e37c6743dd11d45148c4b993
Yazid Bintang <yazidbintang2711@gmail.com>
2024-02-18 21:42:08 +0700

    Development: create add data page

---
+++ lib/add_data_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddDataPage extends StatelessWidget {
+  const AddDataPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Notes"),
+      ),
+      body: Center(child: Text("Test")),
+    );
+  }
+}

33f8850986382ea1ac93c187825756ca323a94d1
Yazid Bintang <yazidbintang2711@gmail.com>
2024-03-05 23:28:54 +0700

    Development: Success edit data from detail page

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,10 +1,24 @@
-import 'package:basic_notes_app/edit_data_page.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   final Map<String, dynamic> data;
   const DetailPage({super.key, required this.data});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  TextEditingController controllerTitle = TextEditingController();
+  TextEditingController controllerNote = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    controllerTitle = TextEditingController(text: widget.data['title']);
+    controllerNote = TextEditingController(text: widget.data['note']);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,34 +26,33 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Detail"),
         actions: [
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange.shade400,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24))),
+          IconButton(
               onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const EditDataPage()));
+                Get.back(result: widget.data);
               },
-              child: Text(
-                "Edit",
-                style: GoogleFonts.inter(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white),
-              )),
-          const SizedBox(
-            width: 16,
-          )
+              icon: const Icon(Icons.edit_document)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.delete))
         ],
       ),
-      body: Center(
+      body: Container(
+        margin: const EdgeInsets.only(top: 16, left: 20, right: 20),
         child: Column(
           children: [
-            Text(data['title']),
-            Text(data['note']),
+            TextField(
+              controller: controllerTitle,
+              onChanged: (value) => widget.data['title'] = value,
+              decoration: const InputDecoration(
+                  border: UnderlineInputBorder(), label: Text("Title")),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: controllerNote,
+              onChanged: (value) => widget.data['note'] = value,
+              decoration: const InputDecoration(
+                  border: InputBorder.none, label: Text("Note")),
+            )
           ],
         ),
       ),
